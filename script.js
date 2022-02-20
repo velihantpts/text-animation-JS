97de286f26d44739a33fb8ae12f5e3b824ae02e1
Velihan <velihantpts@gmail.com>
2022-02-20 23:05:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const textEl = document.getElementById('text')
+const speedEl = document.getElementById('speed')
+
+const text = 'Merhaba Hosgeldiniz'
+let idx = 1
+let speed = 300 / speedEl.value
+
+writeText()
+
+function writeText(){
+
+    textEl.innerText = text.slice(0,idx)
+    idx++
+    
+    if(idx > text.length){
+        idx=1
+    }
+
+    setTimeout(writeText,speed)
+
+}
